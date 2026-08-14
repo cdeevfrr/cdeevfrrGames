8969742e12a1e4056076926972e99bba04795bd4
Danny Rivers <cdeevfrr@gmail.com>
2026-08-14 11:38:51 -0400

    Comment out unused code for game1

--- frontend/src/GameOne/GameOne.tsx
+++ frontend/src/GameOne/GameOne.tsx
@@ -11,61 +11,61 @@ export function GameOne({user}:{user: User}) {
     return <div>
         <h1>Temporarily turned off.</h1>
     </div>
-    const [activeCutsceneIndex, setActiveCutsceneIndex] = useState<keyof typeof cutscenes | null>(null);
-    const [gameState, setGameState] = useState<GameState>(createBaseGameState());
+    // const [activeCutsceneIndex, setActiveCutsceneIndex] = useState<keyof typeof cutscenes | null>(null);
+    // const [gameState, setGameState] = useState<GameState>(createBaseGameState());
 
-    useEffect(() => {
-        // Load game state from database.
-        // If not found, create the starting game state.
-    }, [])
+    // useEffect(() => {
+    //     // Load game state from database.
+    //     // If not found, create the starting game state.
+    // }, [])
 
-    // Play the intro if needed.
-    if (!activeCutsceneIndex && gameState.playerInfo.currentView === "base" && !gameState.playerInfo.viewedCutscenes.has('intro')){
-        setActiveCutsceneIndex('intro');
-    }
+    // // Play the intro if needed.
+    // if (!activeCutsceneIndex && gameState.playerInfo.currentView === "base" && !gameState.playerInfo.viewedCutscenes.has('intro')){
+    //     setActiveCutsceneIndex('intro');
+    // }
     
-    return <div
-        style= {{
-            backgroundImage: 'url(/assets/backgrounds/Gemini_Generated_Cave.png)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            height: '100vh', // Ensure the div has height to actually show the image
-            width: '100vw'
-        }}
-    >
-        {gameState.playerInfo.currentView === 'base' && <BaseView 
-            onCombineGems={() => {}}
-            onEnterDungeon={() => {}}
-            onOpenInventory={() => {}}
-            onOpenPartyMenu={() => {}}
-            playerInfo={gameState.playerInfo}
-        />}
-        {gameState.playerInfo.currentView === 'dungeon' && <DungeonView 
-            dungeonInfo={gameState.dungeonInfo!} 
-            playerInfo={gameState.playerInfo}
-        />}
-        {gameState.playerInfo.currentView === 'battle' && <BattleView 
-            dungeonInfo={gameState.dungeonInfo!} 
-            playerInfo={gameState.playerInfo}
-            battleInfo={gameState.battleInfo!} 
-        />}
+    // return <div
+    //     style= {{
+    //         backgroundImage: 'url(/assets/backgrounds/Gemini_Generated_Cave.png)',
+    //         backgroundSize: 'cover',
+    //         backgroundPosition: 'center',
+    //         height: '100vh', // Ensure the div has height to actually show the image
+    //         width: '100vw'
+    //     }}
+    // >
+    //     {gameState.playerInfo.currentView === 'base' && <BaseView 
+    //         onCombineGems={() => {}}
+    //         onEnterDungeon={() => {}}
+    //         onOpenInventory={() => {}}
+    //         onOpenPartyMenu={() => {}}
+    //         playerInfo={gameState.playerInfo}
+    //     />}
+    //     {gameState.playerInfo.currentView === 'dungeon' && <DungeonView 
+    //         dungeonInfo={gameState.dungeonInfo!} 
+    //         playerInfo={gameState.playerInfo}
+    //     />}
+    //     {gameState.playerInfo.currentView === 'battle' && <BattleView 
+    //         dungeonInfo={gameState.dungeonInfo!} 
+    //         playerInfo={gameState.playerInfo}
+    //         battleInfo={gameState.battleInfo!} 
+    //     />}
 
-        {activeCutsceneIndex && (
-            <CutsceneManager 
-                cutscene={cutscenes[activeCutsceneIndex]} 
-                onComplete={() => {
-                    setGameState((gameState) => {
-                        return {
-                            ...gameState,
-                            playerInfo: {
-                                ...gameState.playerInfo,
-                                viewedCutscenes: new Set([...gameState.playerInfo.viewedCutscenes, activeCutsceneIndex])
-                            }
-                        }
-                    })
-                    setActiveCutsceneIndex(null)
-                }} 
-            />
-        )}
-    </div>
+    //     {activeCutsceneIndex && (
+    //         <CutsceneManager 
+    //             cutscene={cutscenes[activeCutsceneIndex]} 
+    //             onComplete={() => {
+    //                 setGameState((gameState) => {
+    //                     return {
+    //                         ...gameState,
+    //                         playerInfo: {
+    //                             ...gameState.playerInfo,
+    //                             viewedCutscenes: new Set([...gameState.playerInfo.viewedCutscenes, activeCutsceneIndex])
+    //                         }
+    //                     }
+    //                 })
+    //                 setActiveCutsceneIndex(null)
+    //             }} 
+    //         />
+    //     )}
+    // </div>
 }
